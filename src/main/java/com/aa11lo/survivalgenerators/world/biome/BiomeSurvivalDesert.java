95e947cbbe55257d61d17bd5e3e0e0d5fe521d38
lostjarred <lostotaku@outlook.com>
2021-02-05 19:18:58 +1100

    Created BiomeSurvivalDesert Class

---
+++ src/main/java/com/aa11lo/survivalgenerators/world/biome/BiomeSurvivalDesert.java
@@ -0,0 +1,10 @@
+package com.aa11lo.survivalgenerators.world.biome;
+
+public class BiomeSurvivalDesert extends BiomeSurvival {
+
+	public BiomeSurvivalDesert(BiomeProperties properties) {
+		super(properties);
+		// TODO Auto-generated constructor stub
+	}
+
+}
