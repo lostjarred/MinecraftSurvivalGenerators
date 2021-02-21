f7b4c9739cf17186004c3f824520cdf1e64977f6
lostjarred <lostotaku@outlook.com>
2021-02-21 19:15:13 +1100

    add customised surface builder config and stone replacement code

--- src/main/java/com/aa11lo/survivalgenerators/world/biome/BiomeSurvivalSnow.java
+++ src/main/java/com/aa11lo/survivalgenerators/world/biome/BiomeSurvivalSnow.java
@@ -1,9 +1,17 @@
 package com.aa11lo.survivalgenerators.world.biome;
 
+import java.util.Random;
+
+import com.aa11lo.survivalgenerators.world.gen.surfacebuilder.ModSurfaceBuilder;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 
 public class BiomeSurvivalSnow extends BiomeSurvival {
@@ -11,7 +19,7 @@ public class BiomeSurvivalSnow extends BiomeSurvival {
 	public BiomeSurvivalSnow(String registryname) {
 		super(
 				(new Biome.Builder()
-						.surfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG)
+						.surfaceBuilder(SurfaceBuilder.DEFAULT, ModSurfaceBuilder.SURVIVALSNOWSURFACECONFIG)
 						.precipitation(Biome.RainType.SNOW)
 						.category(Biome.Category.TAIGA)
 						.depth(0.2F)
@@ -33,5 +41,19 @@ public class BiomeSurvivalSnow extends BiomeSurvival {
 	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SLIME, 100, 4, 4));
 	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.ENDERMAN, 10, 1, 4));
 	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.WITCH, 5, 1, 1));
+	}
+	@Override
+	public void buildSurface(Random random, IChunk chunkIn, int x, int z, int startHeight, double noise, BlockState defaultBlock, BlockState defaultFluid, int seaLevel, long seed) {
+		super.buildSurface(random, chunkIn, x, z, startHeight, noise, defaultBlock, defaultFluid, seaLevel, seed);
+		int blockx = x;
+		int blockz = z;
+		for (int blocky = startHeight; blocky > 0; blocky--) {
+			if (blocky > 26) {
+				BlockPos curblockpos = new BlockPos(blockx, blocky, blockz);
+				if(chunkIn.getBlockState(curblockpos) == Blocks.STONE.getDefaultState()) {
+					chunkIn.setBlockState(curblockpos, Blocks.PACKED_ICE.getDefaultState(), false);
+				}
+			}
+		}
 	}
 }
