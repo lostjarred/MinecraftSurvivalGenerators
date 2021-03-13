c40b22a93e532091b3fcc582b028e3e33c210bdb
lostjarred <lostone1993@hotmail.com>
2021-03-13 13:42:33 +1100

    added cobble base to light tower

--- src/main/java/com/aa11lo/survivalgenerators/world/gen/WorldGenLightTower.java
+++ src/main/java/com/aa11lo/survivalgenerators/world/gen/WorldGenLightTower.java
@@ -13,6 +13,7 @@ import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class WorldGenLightTower extends WorldGenerator {
 	//blocks used in structure 
+	private final IBlockState cobble = Blocks.COBBLESTONE.getDefaultState();
 	private final IBlockState stone = Blocks.STONE.getDefaultState();
 	private final IBlockState stonebrick = Blocks.STONEBRICK.getDefaultState();
 	private final IBlockState stoneslab = Blocks.STONE_SLAB.getDefaultState().withProperty(BlockSlab.HALF, BlockSlab.EnumBlockHalf.BOTTOM);
@@ -34,7 +35,7 @@ public class WorldGenLightTower extends WorldGenerator {
 		int z = position.getZ();
 		//debug
         SurvivalGeneratorsMain.LOGGER.info("Attempting to spawn light tower at" + " " + x + " " + y + " " + z);
-		if (canSpawnHere(worldIn, x, y, z, 7, 11, 7)) {
+		if (canSpawnHere(worldIn, x, (y + 1), z, 7, 11, 7)) {
 			createBase(worldIn, x, y, z);
 			createPlatform(worldIn, x, (y + 10), z);
 			createTop(worldIn, x, (y + 11), z);
@@ -49,7 +50,7 @@ public class WorldGenLightTower extends WorldGenerator {
 	
 	private boolean canSpawnHere(World worldIn, int posx, int posy, int posz, int sizex, int sizey, int sizez) {
 		boolean canspawn = true;
-		for(int y = posx; y < (posy + sizey); y ++) {
+		for(int y = posy; y < (posy + sizey); y ++) {
 			for(int x = posx; x < (posx + sizex); x ++) {
 				for(int z = posz; z < (posz + sizez); z ++) {
 					if (worldIn.getBlockState(new BlockPos(x, y, z)) != Blocks.AIR.getDefaultState()) {
@@ -63,6 +64,17 @@ public class WorldGenLightTower extends WorldGenerator {
 	}
 	
 	void createBase(World worldIn, int xin, int yin, int zin){
+		
+		//create support base
+		for(int y = (yin - 5); y < yin; y++) {
+			for(int x = xin; x < (xin + 7); x++) {
+				for(int z = zin; z < (zin + 7); z++) {
+					if (worldIn.getBlockState(new BlockPos(x, y, z)) == Blocks.AIR.getDefaultState()) {
+						worldIn.setBlockState(new BlockPos(x - 2, y, z - 2), cobble);
+					}
+				}
+			}
+		}
 		
 		//create stone base
 		for(int x = xin; x < (xin + 7); x++) {
